21aa2308ccdbc638cb3e84ffd63ed7121be29cd1
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-02 01:49:16 +0600

    added socket.io

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import cors from "cors";
+import "dotenv/config";
+import express, { Application, Request, Response } from "express";
+import path from "path";
+const app: Application = express();
+
+/* middleware  */
+app.use(cors());
+app.use(express.json());
+
+/* here will be all the imports routes */
+
+/* here will be the all the routes */
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+/* Here is the User Routes */
+
+// 404 response
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).send({
+    message: "Not Found",
+    status: 404,
+  });
+});
+export { app };
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import http from "http";
+import { Server as WebSocketServer } from "socket.io";
+import { app } from "./app";
+import dbConnect from "./Utils/dbConnect";
+
+const port: string | number = process.env.PORT || 5000;
+const server = http.createServer(app);
+const io = new WebSocketServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const startServer = async (): Promise<void> => {
+  try {
+    await dbConnect();
+    server.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+startServer();
