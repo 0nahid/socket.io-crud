679f1fb27d4ac3b39a37acf34b28b48619acc7e7
Toufiq Hasan  Kiron <toufiqhasankiron0@gmail.com>
2022-10-04 02:27:17 +0600

    fixed uploder route

--- src/Routes/userRouter.ts
+++ src/Routes/userRouter.ts
@@ -1,3 +1,4 @@
+import { uploader } from './../middleware/uploader';
 import { Router } from "express";
 import { userRouter } from "../Controllers/userController";
 const router: Router = Router();
@@ -6,6 +7,6 @@ router.route("/").get(userRouter.getUser).post(userRouter.createUser);
 
 router.route("/:id").patch(userRouter.updateUser).delete(userRouter.deleteUser);
 
-router.route("/upload").post(userRouter.uploadProductImage);
+router.route("/upload").post(uploader.array("image"), userRouter.uploadProductImage);
 
 export default router;
